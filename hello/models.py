ccd4b4fa8834cd1e5271f19688a439ec76c33af1
Patrick NIYONGIRA <niyongirapatrick29@gmail.com>
2021-12-24 22:37:32 +0200

    git 4

--- hello/models.py
+++ hello/models.py
@@ -1,7 +1,19 @@
 from django.db import models
-from django.db.models import CheckConstraint
+#from django.db.models import CheckConstraint
 from PIL import Image
 
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+
+class Genre(models.Model):  
+    genreId = models.AutoField( primary_key=True, serialize=True)  
+    name = models.CharField(max_length=100)  
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:  
+        db_table = "genre"
+    def __str__(self):
+        return "{}".format(self.name)
