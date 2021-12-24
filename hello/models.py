9b6b928457455da38c8826e7fafe6e824ff41e5a
Patrick NIYONGIRA <niyongirapatrick29@gmail.com>
2021-12-24 23:56:38 +0200

    git 4

--- hello/models.py
+++ hello/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 #from django.db.models import CheckConstraint
-#from PIL import Image
+from PIL import Image
 
 # Create your models here.
 class Greeting(models.Model):
