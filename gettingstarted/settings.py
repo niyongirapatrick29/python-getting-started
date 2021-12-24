432536bf2f19fb45bf6106223aecbb717bbb3c7d
Patrick NIYONGIRA <niyongirapatrick29@gmail.com>
2021-12-25 01:24:33 +0200

    git 7

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-#MEDIA_ROOT=os.path.join(BASE_DIR,'img')
+MEDIA_ROOT=os.path.join(BASE_DIR,'img')
 #MEDIA_URL='/img/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
