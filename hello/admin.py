ed0b1733f666ddd201c1a01292969973a1226d7a
Patrick NIYONGIRA <niyongirapatrick29@gmail.com>
2021-12-28 13:17:15 +0200

    django

--- hello/admin.py
+++ hello/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+admin.site.site_header = "Movie Library"
+admin.site.site_title = "Movie Library"
+admin.site.index_title = "Welcome to Movie Library Portal"
 
 admin.site.register(Genre);
 admin.site.register(Actor);
